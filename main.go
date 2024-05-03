11a84c69d4d48d2d3b6230bb64fe33ab8146643d
earentir <earentir@gmail.com>
2024-05-03 15:21:15 +0300

    Very basic mDNS support

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"log"
+	"net"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/bettercap/readline"
@@ -22,6 +24,8 @@ func main() {
 	clientMode := app.BoolOpt("c client-mode", false, "Run in client mode (connect to UNIX socket)")
 	apiMode := app.BoolOpt("a api", false, "Enable the REST API")
 	apiport := app.StringOpt("apiport", "8080", "Port for the REST API")
+	mdnsMode := app.BoolOpt("m mdns", false, "Enable mDNS server")
+	mdnsPort := app.StringOpt("m mdns-port", "5353", "Port for mDNS server")
 
 	app.Action = func() {
 		//if we run in client mode we dont need to run the rest of the code
@@ -63,6 +67,11 @@ func main() {
 			}
 		}()
 
+		// mDNS server setup
+		if *mdnsMode {
+			go startMDNSServer(*mdnsPort)
+		}
+
 		// If running in daemon mode, exit after starting the server
 		if *daemon {
 			if *remoteUnix != "" {
@@ -93,6 +102,35 @@ func main() {
 	err := app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
+	}
+}
+
+func startMDNSServer(port string) {
+	portInt, _ := strconv.Atoi(port)
+
+	// Set up the multicast address for mDNS
+	addr := &net.UDPAddr{IP: net.ParseIP("224.0.0.251"), Port: portInt}
+
+	// Create a UDP connection to listen on multicast address
+	conn, err := net.ListenMulticastUDP("udp4", nil, addr)
+	if err != nil {
+		log.Fatalf("Error setting up mDNS server: %v", err)
+	}
+
+	// Set reuse address option to allow multiple listeners on the same address
+	if err := conn.SetReadBuffer(65535); err != nil {
+		log.Fatalf("Failed to set read buffer size: %v", err)
+	}
+
+	server := &dns.Server{
+		PacketConn: conn,
+	}
+
+	dns.HandleFunc("local.", handleMDNSRequest)
+
+	log.Printf("Starting mDNS server on %s\n", addr)
+	if err := server.ActivateAndServe(); err != nil {
+		log.Fatalf("Error starting mDNS server: %v", err)
 	}
 }
 
