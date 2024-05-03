66dcfae3b0771aa8b0800f67c1e23dc7b5cf2081
earentir <earentir@gmail.com>
2024-05-03 15:13:47 +0300

    added fields for dates and MAC for the future

--- structs.go
+++ structs.go
@@ -34,11 +34,14 @@ type DNSServerSettings struct {
 
 // DNSRecord holds the data for a DNS record
 type DNSRecord struct {
-	Name      string    `json:"name"`
-	Type      string    `json:"type"`
-	Value     string    `json:"value"`
-	TTL       uint32    `json:"ttl"`
-	LastQuery time.Time `json:"last_query"`
+	Name       string    `json:"name"`
+	Type       string    `json:"type"`
+	Value      string    `json:"value"`
+	TTL        uint32    `json:"ttl"`
+	AddedOn    time.Time `json:"added_on,omitempty"`
+	UpdatedOn  time.Time `json:"updated_on,omitempty"`
+	MACAddress string    `json:"mac,omitempty"`
+	LastQuery  time.Time `json:"last_query,omitempty"`
 }
 
 // Servers holds the data for the servers
