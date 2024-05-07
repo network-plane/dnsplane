20c37e26bd4853e9ffd0639d1b17b473227336e9
earentir <earentir@gmail.com>
2024-05-07 10:18:52 +0300

    moved from main

--- helpers.go
+++ helpers.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"log"
+	"os"
+)
+
+func createFileIfNotExists(filename, content string) {
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		err = os.WriteFile(filename, []byte(content), 0644)
+		if err != nil {
+			log.Fatalf("Error creating %s: %s", filename, err)
+		}
+	}
+}
