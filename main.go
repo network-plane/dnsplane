027e21b46ed24758e9b2e04407001fd8c3d5f049
earentir <earentir@gmail.com>
2024-05-07 11:57:26 +0300

    renamed server settings struct

--- main.go
+++ main.go
@@ -117,12 +117,12 @@ func initializeJSONFiles() {
 }
 
 // loadSettings reads the dnsresolver.json file and returns the DNS server settings
-func loadSettings() DNSServerSettings {
-	return data.LoadFromJSON[DNSServerSettings]("dnsresolver.json")
+func loadSettings() DNSResolverSettings {
+	return data.LoadFromJSON[DNSResolverSettings]("dnsresolver.json")
 }
 
 // saveSettings saves the DNS server settings to the dnsresolver.json file
-func saveSettings(settings DNSServerSettings) {
+func saveSettings(settings DNSResolverSettings) {
 	if err := data.SaveToJSON("dnsresolver.json", settings); err != nil {
 		log.Fatalf("Failed to save settings: %v", err)
 	}
