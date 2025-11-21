7e393201b7515c7ca00802d2bee4123fa885c1b0
earentir <earentir@gmail.com>
2025-11-21 14:57:52 +0200

    upstream package

---
+++ resolver/upstream.go
@@ -0,0 +1,36 @@
+package resolver
+
+import (
+	"context"
+	"time"
+
+	"github.com/miekg/dns"
+)
+
+// DNSClient implements UpstreamClient using github.com/miekg/dns.
+type DNSClient struct {
+	client *dns.Client
+}
+
+// NewDNSClient creates an upstream client with the desired dial timeout.
+func NewDNSClient(timeout time.Duration) *DNSClient {
+	if timeout <= 0 {
+		timeout = 2 * time.Second
+	}
+	return &DNSClient{
+		client: &dns.Client{
+			Timeout: timeout,
+		},
+	}
+}
+
+// Query sends the question to the specified DNS server.
+func (c *DNSClient) Query(ctx context.Context, question dns.Question, server string) (*dns.Msg, error) {
+	if c == nil || c.client == nil {
+		return nil, nil
+	}
+	message := new(dns.Msg)
+	message.SetQuestion(question.Name, question.Qtype)
+	resp, _, err := c.client.ExchangeContext(ctx, message, server)
+	return resp, err
+}
